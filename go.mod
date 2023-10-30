a29d2e5b284cac189c26788ceb77ecc686e75a0c
digua <digua@gmail.com>
2023-10-30 20:58:42 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/frankcatox/logger
+
+go 1.15
+
+require (
+	github.com/fatih/color v1.10.0
+	github.com/mailru/easyjson v0.7.6
+	github.com/mattn/go-isatty v0.0.12
+)
---
+++ root.go
@@ -0,0 +1,165 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/mattn/go-isatty"
+	"os"
+	"strings"
+)
+
+var (
+	log *Logger
+)
+
+func init() {
+	log = NewLogger()
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		log.Detach("console")
+		config := &ConsoleConfig{Color: true}
+		log.Attach("console", LOGGER_LEVEL_DEBUG, config)
+	}
+}
+
+func Detach(adapterName string) error {
+	return log.Detach(adapterName)
+}
+
+func Attach(adapterName string, level int, config Config) error {
+	return log.Attach(adapterName, level, config)
+}
+
+func SetLevel(level string) {
+	for _, output := range log.outputs {
+		output.Level = log.LoggerLevel(strings.ToUpper(level))
+	}
+}
+
+func LoggerLevel(level string) int {
+	return log.LoggerLevel(strings.ToUpper(level))
+}
+
+func Emergency(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_EMERGENCY, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_EMERGENCY, msg)
+	}
+}
+
+func Emergencyf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_EMERGENCY, msg)
+}
+
+func Alert(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_ALERT, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_ALERT, msg)
+	}
+}
+
+func Alertf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_ALERT, msg)
+}
+
+func Critical(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_CRITICAL, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_CRITICAL, msg)
+	}
+}
+
+func Criticalf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_CRITICAL, msg)
+}
+
+func Error(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_ERROR, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_ERROR, msg)
+	}
+}
+
+func Errorf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_ERROR, msg)
+}
+
+func Warning(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_WARNING, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_WARNING, msg)
+	}
+}
+
+func Warningf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_WARNING, msg)
+}
+
+func Warn(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_WARNING, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_WARNING, msg)
+	}
+}
+
+func Warnf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_WARNING, msg)
+}
+
+func Notice(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_NOTICE, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_NOTICE, msg)
+	}
+}
+
+func Noticef(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_NOTICE, msg)
+}
+
+func Info(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_INFO, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_INFO, msg)
+	}
+}
+
+func Infof(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_INFO, msg)
+}
+
+func Debug(msg string, a ...interface{}) {
+	if len(a) > 0 {
+		msg := fmt.Sprintf(msg, a...)
+		log.Writer(LOGGER_LEVEL_DEBUG, msg)
+	} else {
+		log.Writer(LOGGER_LEVEL_DEBUG, msg)
+	}
+}
+
+func Debugf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	log.Writer(LOGGER_LEVEL_DEBUG, msg)
+}
